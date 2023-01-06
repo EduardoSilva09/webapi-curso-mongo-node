212bec1f5a8eb3d4b483c43a05e20b1b01524010
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-06 18:08:57 -0300

    Initial Commit

---
+++ db.js
@@ -0,0 +1,56 @@
+const { v4 } = require('uuid')
+const fs = require('fs')
+const path = require('path')
+const FILE_PATH = path.join(__dirname, 'users.json')
+
+function findUsers() {
+    try {
+        return require('./users.json')
+    } catch (error) {
+        return []
+    }
+}
+
+function findUser(id) {
+    return findUsers().find(item => item.id === id)
+}
+
+function insertUser(user) {
+    const users = findUsers()
+    user.id = v4()
+    users.push(user)
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users))
+    return user
+}
+
+function updateUser(id, user) {
+    const users = findUsers()
+
+    users.forEach((item, indice, array) => {
+        if (item.id === id) {
+            user.id = id
+            array[indice] = user
+        }
+    });
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users))
+    return user
+}
+
+function deleteUser(id) {
+    const users = findUsers()
+    users.forEach((item, indice, array) => {
+        if (item.id === id) {
+            array.splice(indice, 1)
+        }
+    });
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users))
+    return id
+}
+
+module.exports = {
+    findUsers,
+    findUser,
+    insertUser,
+    updateUser,
+    deleteUser
+}
