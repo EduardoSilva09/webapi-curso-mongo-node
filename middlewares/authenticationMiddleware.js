67d84da5802b748c23a50daf1e8ba40ca96d9fc8
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-08 15:05:27 -0300

    Refatorado middleware de autenticação, para validar autorização de acesso a partir da api key

--- middlewares/authenticationMiddleware.js
+++ middlewares/authenticationMiddleware.js
@@ -1,6 +1,9 @@
+const keyModel = require('../model/keyModel')
+
 module.exports = (req, res, next) => {
     const authorization = req.headers['authorization']
-    if (authorization === '123') {
+    const apiKey = keyModel.findKey(authorization.replace('ApiKey ', ''))
+    if (apiKey && apiKey.enabled) {
         return next()
     } else
         res.sendStatus(401)// Não Autorizado
