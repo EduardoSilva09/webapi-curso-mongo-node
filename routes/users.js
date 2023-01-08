beedbb0d1e70acfb32810011d2860110a88c0e11
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-08 15:44:51 -0300

    removida referencia de userModel que não estava sendo utilizada

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../model/userModel');
 const userController = require('../controllers/userController')
 const validationUserMiddleware = require('../middlewares/validationMidleware')
 
